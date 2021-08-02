7e988be263f0920a736875a9635f7f450d2e5e6a
pengfei <liu.pengfei@hotmail.fr>
2021-08-02 10:51:12 +0200

    update command shell and requirements for tensor worker

---
+++ bash_command/install_requirement_on_tensor_worker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pip install -r ../requirements/tensor_requirements.txt
--- bash_command/train_model_command.sh
+++ bash_command/train_model_command.sh
@@ -1,7 +1,8 @@
-pip install -r requirements.txt
+#!/bin/bash
 
-export MLFLOW_TRACKING_URI='https://pengfei-mlflow-5806178270327072668-mlflow-ihm.kub.sspcloud.fr'
+export MLFLOW_TRACKING_URI='https://pengfei-mlflow-6923080064718463498-mlflow-ihm.kub.sspcloud.fr'
 export MLFLOW_EXPERIMENT_NAME="openfood"
 export MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
 
+# optional -n <number of epochs> -l <learning rate>
 python run_pretrained_embedding_with_mlflow.py -c config.yml
